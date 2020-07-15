3f7714316299ad1620bf98de6223551c09afba36
fakeboyas <ovoivi9@gmail.com>
2020-07-15 16:49:18 +0700

    edit port

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const PORT =5001
-const {db} = require('./config')
+const {PORT} = require('./config')
+const { db } = require("./config");
 
-app.use(express.json())
-app.use(express.urlencoded(({extended:false})))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/movies', require('./routes/movies'));
+app.use("/api/movies", require("./routes/movies"));
 
-app.get('/', (req,res)=>{
-    res.render('pages/index.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("pages/index.ejs");
+});
 
-if(db){
-    console.log ('connect to database')
-    app.listen(PORT,()=>    {
-        console.log(`Server running on port ${PORT}`)
-    })
+if (db) {
+  console.log("connect to database");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
